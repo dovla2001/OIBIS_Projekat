35bd63d28b745e0b559f86d2da2b8d120d6c7fea
dovla2001 <vmandic83@gmail.com>
2023-10-23 14:06:52 +0200

    Kostur projekta

---
+++ Projekat/OIBISProjekat/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Common;
+using System.ServiceModel.Channels;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<ITest> channelTest = new ChannelFactory<ITest>("Test");
+            ITest proxyTest = channelTest.CreateChannel();
+
+            string tekst = proxyTest.Proba();
+            Console.WriteLine(tekst);
+            Console.Read();
+        }
+    }
+}
