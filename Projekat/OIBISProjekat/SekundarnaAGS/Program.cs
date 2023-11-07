b9e8995e5735fcf21f8b02d3bc89026a3ed44dda
Pjur9 <116374807+Pjur9@users.noreply.github.com>
2023-11-07 23:30:17 +0100

    update SekundarnaAGS

---
+++ Projekat/OIBISProjekat/SekundarnaAGS/Program.cs
@@ -0,0 +1,33 @@
+﻿using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SekundarnaAGS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string srvCertCN = "PrimarnaAGS";
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/ISecurityService"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            using (WCFClient proxy = new WCFClient(binding, address))
+            {
+                proxy.TestCommunication();
+                Console.WriteLine("TestCommunication() finished. Press <enter> to continue ...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
