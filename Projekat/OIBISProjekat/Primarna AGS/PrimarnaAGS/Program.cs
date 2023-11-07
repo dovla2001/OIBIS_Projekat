e18a773bd14efafa4ab18e866759355a19340e74
Pjur9 <116374807+Pjur9@users.noreply.github.com>
2023-11-07 23:27:52 +0100

    update PrimarnaAGS

---
+++ Projekat/OIBISProjekat/Primarna AGS/PrimarnaAGS/Program.cs
@@ -0,0 +1,57 @@
+﻿using Contracts;
+using Manager;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+
+namespace PrimarnaAGS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string srvCertCN = Formater.ParseName(WindowsIdentity.GetCurrent().Name);
+
+            NetTcpBinding binding = new NetTcpBinding();
+            NetTcpBinding bindingWindows = new NetTcpBinding();
+
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            bindingWindows.Security.Mode = SecurityMode.Transport;
+            bindingWindows.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            bindingWindows.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            string address = "net.tcp://localhost:9999/ISecurityService";
+            string addressWindows = "net.tcp://localhost:9998/ISecurityService";
+
+            ServiceHost host = new ServiceHost(typeof(WCFService));
+
+            host.AddServiceEndpoint(typeof(IWCFContracts), binding, address);
+            host.AddServiceEndpoint(typeof(IWCFContracts), bindingWindows, addressWindows);
+
+            try
+            {
+                host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+                host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+                host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+                host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+                host.Open();
+                Console.WriteLine("WCFService is started. Press <enter> to stop ...");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
+
+
+        }
+    }
+}
